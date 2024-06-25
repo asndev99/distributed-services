dd88a97ee5cddeae1e59da34e1e1261b328ca340
asndev <asndev99@gmail.com>
2024-06-25 15:47:46 +0500

    new api added for getting all users with required projection

--- auth-service/controller/userController.js
+++ auth-service/controller/userController.js
@@ -8,11 +8,42 @@ class UserController {
         where: {
           id: id,
         },
+        select: {
+          id: true,
+          email: true,
+          name: true,
+        },
       });
       return res.status(200).json({
         success: true,
         payload: user,
         message: null,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getUsers(req, res) {
+    try {
+      const { userIds } = req.body;
+      console.log(userIds);
+      const users = await prisma.user.findMany({
+        where: {
+          id: {
+            in: userIds,
+          },
+        },
+        select: {
+          id: true,
+          email: true,
+          name: true,
+        },
+      });
+      return res.status(200).json({
+        success: true,
+        payload: users,
+        message: "null",
       });
     } catch (error) {
       next(error);
--- auth-service/routes/userRoutes.js
+++ auth-service/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const UserController = require("../controller/userController");
 const userRouter = require("express").Router();
 
+userRouter.get("/all", UserController.getUsers);
 userRouter.get("/:id", UserController.getUser);
 
 module.exports = userRouter;
